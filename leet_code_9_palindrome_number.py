ef01183e3355201d07c0bab36a2c8c7ef6f9bbb0
jrodr117 <jrodr113@msudenver.edu>
2023-09-29 09:08:54 -0600

    solved Palindrome Number Leet Code Problem

--- leet_code_9_palindrome_number.py
+++ leet_code_9_palindrome_number.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        # casts number arg into type string and stores it in string_num variable
+        string_num = str(x)
+        # slicies  string num in reverse and stores it in reversed_num var
+        reversed_num = string_num[::-1]
+        # compares string_num to reversed_num and returns True if num is a palindrome and False if it isn't
+        if string_num == reversed_num:
+            return True
+        else:
+            return False
